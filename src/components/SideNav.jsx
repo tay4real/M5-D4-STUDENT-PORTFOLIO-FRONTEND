68beddee2a8eaf6684de40d2b1050dea722a2af0
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-11 03:07:21 +0100

    Create SideNav.jsx

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Nav, ListGroup } from "react-bootstrap";
+
+export default function SideNav(props) {
+  return (
+    <Nav
+      variant="pills"
+      defaultActiveKey="/home"
+      className="flex-column pt-5 "
+      style={{ minHeight: "85vh" }}
+    >
+      <Nav.Item>
+        <Nav.Link className="text-white" href="/home">
+          All Students
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link className="text-white" eventKey="link-1">
+          Manage Student
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link className="text-white" eventKey="link-2">
+          Project List
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link className="text-white" eventKey="link-3">
+          Manage Projects
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
