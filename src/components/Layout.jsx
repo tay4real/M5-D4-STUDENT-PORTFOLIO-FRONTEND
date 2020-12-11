a43ff65df6647cf05f47fec0ab5dc2609bf192dd
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-11 03:07:17 +0100

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import HeaderNav from "./HeaderNav";
+import SideNav from "./SideNav";
+
+export default class Layout extends Component {
+  state = {
+    isNavOpen: true,
+  };
+
+  onNavToggle = () => {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  };
+  render() {
+    const { isNavOpen } = this.state;
+
+    return (
+      <Container fluid className="p-0 m-0">
+        <HeaderNav
+          isNavOpen={this.state.isNavOpen}
+          onNavToggle={this.onNavToggle}
+        />
+        <Row className="p-0 m-0">
+          {this.state.isNavOpen && (
+            <Col xs={3} className="bg-dark" style={{ maxHeight: "100vh" }}>
+              <SideNav isNavOpen={this.state.isNavOpen} />
+            </Col>
+          )}
+          <Col className="bg-light"></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
