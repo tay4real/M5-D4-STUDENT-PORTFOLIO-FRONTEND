01809fbb633b7786e72cbf7193440babdacfb0ed
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-11 03:07:02 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Layout from "./components/Layout";
+
+function App() {
+  return <Layout />;
+}
+
+export default App;
